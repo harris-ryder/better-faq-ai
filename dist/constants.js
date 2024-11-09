70664d51b05200984245aa2715c23ab9295ff0fd
harris-ryder <ryderharris@icloud.com>
2024-11-09 16:59:21 +0000

    Remove dist directory from version control

--- dist/constants.js
+++
@@ -1,4 +0,0 @@
-export const WEBFLOW_API_URL = "https://api.webflow.com";
-export const WEBFLOW_COLLECTIONS_DN = "better-faqs"; // cms display name
-export const WEBFLOW_COLLECTIONS_SN = "better-faq"; // cms singular name
-//# sourceMappingURL=constants.js.map
